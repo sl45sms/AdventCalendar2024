65815f25b6172c5426c0ed298fa7e9f7442281ab
root <root@skarvelis.gr>
2024-12-03 07:14:44 +0200

    done 3

--- 3/index.js
+++ 3/index.js
@@ -1,13 +1,28 @@
 //Test
-let str = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+let tstr = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
 let regex = /mul\((\d+),(\d+)\)/g;
-let result = str.match(regex);
+let tresult = tstr.match(regex);
 let arr = [];
-for (let i = 0; i < result.length; i++) {
-    let temp = result[i].match(/\d+/g);
+for (let i = 0; i < tresult.length; i++) {
+    let temp = tresult[i].match(/\d+/g);
     temp = temp[0] * temp[1];
     arr.push(temp);
 }
 
 let sum = arr.reduce((a, b) => a + b, 0);
-console.log(sum);
+console.log("Test Sum", sum);
+
+const fs = require('fs')
+const path = require('path')
+const filePath = path.join(__dirname, 'input')
+const str = fs.readFileSync(filePath, 'utf8')
+let result = str.match(regex);
+
+let arr2 = [];
+for (let i = 0; i < result.length; i++) {
+    let temp = result[i].match(/\d+/g);
+    temp = temp[0] * temp[1];
+    arr2.push(temp);
+}
+let sum2 = arr2.reduce((a, b) => a + b, 0);
+console.log("Sum", sum2) 
